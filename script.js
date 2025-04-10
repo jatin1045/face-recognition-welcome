39d62ab2b564a9107dd8a9e1c483b7e08c2dfe0d
Jatin <jatinjoshi1045@gmail.com>
2025-04-11 00:09:00 +0530

    Added Modern Stylish Welcome Page

---
+++ script.js
@@ -0,0 +1,9 @@
+// const toggleBtn = document.getElementById('toggle-mode');
+
+// toggleBtn.addEventListener('click', () => {
+//   document.body.classList.toggle('dark-mode');
+// });
+
+function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+}
